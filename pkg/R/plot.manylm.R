3fc0a5b2bc5cc71725754acb4dc69e5891a2faf0
dwarton <dwarton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-04 01:42:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvabund@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.manylm.R
+++ pkg/R/plot.manylm.R
@@ -5,7 +5,7 @@
 
 plot.manylm  <- function(x, res.type="pearson", which = 1:4, caption = c("Residuals vs Fitted","Normal Q-Q", "Scale-Location", "Cook\'s distance"), overlay=TRUE, n.vars=12, var.subset=NULL, sub.caption = NULL, studentized=TRUE,	... ) 
 {	
-   if (res.type!="pearson") 
+   if ( res.type!="pearson" | res.type!="pit.norm" ) 
       warning("Only Pearson residuals implemented for manylm functions.")
    default.plot.manylm(x, which = which, caption = caption, overlay=overlay, n.vars=n.vars, var.subset=var.subset, sub.caption = sub.caption, studentized=studentized, ... )
    
