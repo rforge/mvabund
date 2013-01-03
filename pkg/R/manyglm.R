948acabd50b379ea403b402bfdbeaf7aa5a6705c
yiw <yiw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-03 05:10:57 +0000

    Changed default values of maxiter to 50 and maxiter2 to 1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvabund@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/manyglm.R
+++ pkg/R/manyglm.R
@@ -3,7 +3,7 @@
 # the (default) methods coef, residuals, fitted values can be used             
 ###############################################################################
 
-manyglm <- function (formula, family="negative.binomial", K=1, data=NULL, subset=NULL, na.action=options("na.action"), theta.method = "PHI", model = FALSE, x = TRUE, y = TRUE, qr = TRUE, cor.type= "I", shrink.param=NULL, tol=sqrt(.Machine$double.eps), maxiter=25, maxiter2=10, show.coef=FALSE, show.fitted=FALSE, show.residuals=FALSE, ... ) {
+manyglm <- function (formula, family="negative.binomial", K=1, data=NULL, subset=NULL, na.action=options("na.action"), theta.method = "PHI", model = FALSE, x = TRUE, y = TRUE, qr = TRUE, cor.type= "I", shrink.param=NULL, tol=sqrt(.Machine$double.eps), maxiter=50, maxiter2=1, show.coef=FALSE, show.fitted=FALSE, show.residuals=FALSE, ... ) {
 
 # tasmX <- as.matrix(tasmX, "numeric")  
 
