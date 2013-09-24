bee57144d035765247e799c56604b50489556e6f
yiw <yiw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-24 09:23:37 +0000

    S3methods for extractAIC.manyglm and nobs.manyglm Aligned with new Imports, Depends rules for external R packages (MASS, Rcpp, methods, stats)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvabund@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/extractAIC.manyglm.R
+++ pkg/R/extractAIC.manyglm.R
@@ -1,8 +1,9 @@
-extractAIC.manyglm<- function(object, k=2, ...)
+extractAIC.manyglm<- function(fit, scale, k=2, ...)
 {
-  n.vars = NCOL(object$y)
-  ll=logLik(object)
+  n.vars = NCOL(fit$y)
+  ll=logLik(fit)
   edf = attr(ll,"df") * n.vars
   AIC = -2*sum(ll) + k * edf
   return( c(edf,AIC) )
+  NextMethod("extractAIC")
 }
--- pkg/R/nobs.manyglm.R
+++ pkg/R/nobs.manyglm.R
@@ -2,4 +2,5 @@ nobs.manyglm<- function(object, ...)
 {
   n.rows = NROW(object$y)
   return( n.rows )
+  NextMethod("nobs")
 }
